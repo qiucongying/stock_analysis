11dd2d3c013477d8ce7a22be963ab75f4f893ea2
Congying Qiu <qiucongying20@gmail.com>
2023-01-26 16:45:06 -0800

    Update sp500.py

--- sp500.py
+++ sp500.py
@@ -3,3 +3,11 @@ import pandas as pd
 companies_data = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
 table = companies_data[0]
 table.head()
+
+sliced_table = table[1:]
+header = table.iloc[0]
+
+corrected_table = sliced_table.rename(columns=header)
+
+tickers = corrected_table['Ticker symbol'].tolist()
+print (tickers)
